c425c894df063f757fd391843c63f58dc42e53da
Spencer Kuzara <kodaxx@gmail.com>
2017-11-12 18:43:46 -0700

    add ravenous part 1

---
+++ ravenous/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+import Business from './components/Business/Business';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
