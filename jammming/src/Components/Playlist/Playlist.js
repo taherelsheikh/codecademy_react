0344df814bb9ca86a79b2f0361608844153c45ee
Spencer Kuzara <kodaxx@gmail.com>
2017-12-04 20:58:35 -0700

    fixed defaultValue to value in Playlist.js

--- jammming/src/Components/Playlist/Playlist.js
+++ jammming/src/Components/Playlist/Playlist.js
@@ -13,7 +13,7 @@ class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-        <input onChange={this.handleNameChange} defaultValue={this.props.playlistName} />
+        <input onChange={this.handleNameChange} value={this.props.playlistName} />
         <TrackList isRemoval={true} onRemove={this.props.onRemove} tracks={this.props.playlistTracks}/>
         <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
       </div>
